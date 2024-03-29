5d270081663179ad87677813569bb7e70dc933a8
AthulAjay <athulajay16@gmail.com>
2024-03-29 18:11:29 +0530

    New

---
+++ project/accounts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def signin(request):
+
+    if request.method=="POST":
+       username=request.POST['username']
+       password = request.POST['password']
+       user=auth.authenticate(username=username, password=password)
+       if user is not None:
+          auth.login(request,user)
+          return redirect('dashboard')
+       else:
+          return redirect('signin')
+    
+    return render(request, 'signin.html')
+
+def signup(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        email=request.POST['email']
+        password = request.POST['password']
+
+        user=User.objects.create_user(username=username,email=email,password=password)
+        user.save();
+        return redirect('signin')
+    
+    else:
+     return render(request,'signup.html') 
+    
+
+def signout(request):
+   auth.logout(request)
+   return redirect('signin')
